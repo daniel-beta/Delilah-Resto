4c3aa7f4198766f94b5f78531c59498b0db15303
Daniel Betancur Rodríguez <danielbetancur@ingenieros.com>
2021-12-30 21:41:03 -0500

    feat: project update

---
+++ routes/users.routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const userControllers = require('../controllers/users.controller')
+const verifyToken = require('../middleware/validateToken.middleware')
+
+router.post('/', [verifyToken.verifyToken, verifyToken.isAdmin], userControllers.createUser)
+router.get('/', [verifyToken.verifyToken, verifyToken.isAdmin], userControllers.getUsers)
+router.get('/:userId', [verifyToken.verifyToken, verifyToken.isAdmin], userControllers.getUsersById)
+router.put('/:userId', [verifyToken.verifyToken, verifyToken.isAdmin], userControllers.updateUsersById)
+router.delete('/:userId', [verifyToken.verifyToken, verifyToken.isAdmin], userControllers.deleteUserById)
+
+module.exports = router
